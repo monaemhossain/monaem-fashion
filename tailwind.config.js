b7c93ac4dbc39f39c3ed6e9b41e867e41d88c39e
monaemhossain <monaemhs@gmail.com>
2023-10-18 08:30:59 +0300

    cleared default content and install flowbite in the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
-
-const withMT = require("@material-tailwind/react/utils/withMT");
- 
-module.exports = withMT({
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
-    "./index.html", 
-    "./src/**/*.{vue,js,ts,jsx,tsx}"
-  ],
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    ],
   theme: {
     extend: {},
   },
-  plugins: [],
-});
+  plugins: [
+    require('flowbite/plugin'),
+  ],
+}
+
