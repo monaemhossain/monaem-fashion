1ffe0d942099cc3bf71a05b9cc8fee3a9f965931
monaemhossain <monaemhs@gmail.com>
2023-10-19 15:25:00 +0300

    intigrated mongodb with our app, now we can store data to the cloud

--- src/Pages/AddProduct/AddProduct.jsx
+++ src/Pages/AddProduct/AddProduct.jsx
@@ -13,16 +13,28 @@ const AddProduct = () => {
         const productType = e.target.productType.value;
         const productPrice = e.target.productPrice.value;
         const productDescription = e.target.productDescription.value;
-        
+        const newProduct = { photo, productName, brandName, productType, productPrice, productDescription }
+        console.log(newProduct);
+
+        // send data to server
+        fetch('http://localhost:5000/products', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newProduct)
+        })
+        .then(res => res.json())
+        .then(data => {console.log(data)})
     }
 
 
     return (
-        <section className='max-w-sm mx-auto md:py-30 py-20'>
+        <section className='max-w-lg mx-auto md:py-30 py-20'>
             <div className='mb-5'>
                 {/* {successMsg ? <Alert severity="success">{successMsg}</Alert> : ''} */}
             </div>
-            <form className="flex max-w-md flex-col gap-4" onSubmit={handleSubmit}>
+            <form className="flex max-w-lg flex-col gap-4" onSubmit={handleSubmit}>
                 <div>
                     <div className="mb-2 block">
                         <Label
@@ -97,18 +109,10 @@ const AddProduct = () => {
                     />
                 </div>
                 <div>
-                    <div className="mb-2 block">
-                        <Label
-                            htmlFor="productDescription"
-                            value="Product Description"
-                        />
-                    </div>
-                    <TextInput
-                        id="productDescription"
-                        required
-                        type="text"
-                        placeholder="Describe your product"
-                    />
+
+                    <label htmlFor="productDescription" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Description</label>
+                    <textarea id="productDescription" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Product Description"></textarea>
+
                 </div>
 
                 <Button type="submit" className='dark:text-white dark:border-white dark:hover:bg-white dark:hover:text-black text-black border-2 border-black hover:bg-darkTheme hover:text-white transition-all'>
